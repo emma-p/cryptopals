9481f45a929533246d6c5c71f70a7288aa6efe70
Emmanuelle Poirier <manuepoirier@gmail.com>
2016-07-25 07:29:51 +0100

    Create HexChar type

---
+++ app/HexChar.hs
@@ -0,0 +1,49 @@
+module HexChar where
+
+data HexChar
+  = Hex0
+  | Hex1
+  | Hex2
+  | Hex3
+  | Hex4
+  | Hex5
+  | Hex6
+  | Hex7
+  | Hex8
+  | Hex9
+  | HexA
+  | HexB
+  | HexC
+  | HexD
+  | HexE
+  | HexF
+  deriving (Eq, Enum, Bounded)
+
+toChar :: HexChar -> Char
+toChar Hex0 = '0'
+toChar Hex1 = '1'
+toChar Hex2 = '2'
+toChar Hex3 = '3'
+toChar Hex4 = '4'
+toChar Hex5 = '5'
+toChar Hex6 = '6'
+toChar Hex7 = '7'
+toChar Hex8 = '8'
+toChar Hex9 = '9'
+toChar HexA = 'a'
+toChar HexB = 'b'
+toChar HexC = 'c'
+toChar HexD = 'd'
+toChar HexE = 'e'
+toChar HexF = 'f'
+
+charHexChar :: [(Char, HexChar)]
+charHexChar = map (\hex -> (toChar hex, hex)) [minBound..maxBound]
+
+fromChar :: Char -> HexChar
+fromChar c =
+  case lookup c charHexChar of
+    Just h -> h
+    _ -> error $ "Error: unexpected character: " ++ show c
+
+type HexString = [HexChar]
--- app/Xor.hs
+++ app/Xor.hs
@@ -1,55 +1,56 @@
 module Xor where
 
 import Data.List.Split
+import HexChar
 
 type BitList = [Bool]
 
-hexToBitList :: Char -> BitList
-hexToBitList '0' = [False, False, False, False]
-hexToBitList '1' = [False, False, False, True]
-hexToBitList '2' = [False, False, True, False]
-hexToBitList '3' = [False, False, True, True]
-hexToBitList '4' = [False, True, False, False]
-hexToBitList '5' = [False, True, False, True]
-hexToBitList '6' = [False, True, True, False]
-hexToBitList '7' = [False, True, True, True]
-hexToBitList '8' = [True, False, False, False]
-hexToBitList '9' = [True, False, False, True]
-hexToBitList 'a' = [True, False, True, False]
-hexToBitList 'b' = [True, False, True, True]
-hexToBitList 'c' = [True, True, False, False]
-hexToBitList 'd' = [True, True, False, True]
-hexToBitList 'e' = [True, True, True, False]
-hexToBitList 'f' = [True, True, True, True]
+hexToBitList :: HexChar -> BitList
+hexToBitList Hex0 = [False, False, False, False]
+hexToBitList Hex1 = [False, False, False, True]
+hexToBitList Hex2 = [False, False, True, False]
+hexToBitList Hex3 = [False, False, True, True]
+hexToBitList Hex4 = [False, True, False, False]
+hexToBitList Hex5 = [False, True, False, True]
+hexToBitList Hex6 = [False, True, True, False]
+hexToBitList Hex7 = [False, True, True, True]
+hexToBitList Hex8 = [True, False, False, False]
+hexToBitList Hex9 = [True, False, False, True]
+hexToBitList HexA = [True, False, True, False]
+hexToBitList HexB = [True, False, True, True]
+hexToBitList HexC = [True, True, False, False]
+hexToBitList HexD = [True, True, False, True]
+hexToBitList HexE = [True, True, True, False]
+hexToBitList HexF = [True, True, True, True]
 hexToBitList _ = error "Invalid character"
 
-bitListToHex :: BitList -> Char
-bitListToHex [False, False, False, False]= '0'
-bitListToHex [False, False, False, True] = '1'
-bitListToHex [False, False, True, False] = '2'
-bitListToHex [False, False, True, True]  = '3'
-bitListToHex [False, True, False, False] = '4'
-bitListToHex [False, True, False, True]  = '5'
-bitListToHex [False, True, True, False]  = '6'
-bitListToHex [False, True, True, True]   = '7'
-bitListToHex [True, False, False, False] = '8'
-bitListToHex [True, False, False, True]  = '9'
-bitListToHex [True, False, True, False]  = 'a'
-bitListToHex [True, False, True, True]   = 'b'
-bitListToHex [True, True, False, False]  = 'c'
-bitListToHex [True, True, False, True]   = 'd'
-bitListToHex [True, True, True, False]   = 'e'
-bitListToHex [True, True, True, True]    = 'f'
+bitListToHex :: BitList -> HexChar
+bitListToHex [False, False, False, False]= Hex0
+bitListToHex [False, False, False, True] = Hex1
+bitListToHex [False, False, True, False] = Hex2
+bitListToHex [False, False, True, True]  = Hex3
+bitListToHex [False, True, False, False] = Hex4
+bitListToHex [False, True, False, True]  = Hex5
+bitListToHex [False, True, True, False]  = Hex6
+bitListToHex [False, True, True, True]   = Hex7
+bitListToHex [True, False, False, False] = Hex8
+bitListToHex [True, False, False, True]  = Hex9
+bitListToHex [True, False, True, False]  = HexA
+bitListToHex [True, False, True, True]   = HexB
+bitListToHex [True, True, False, False]  = HexC
+bitListToHex [True, True, False, True]   = HexD
+bitListToHex [True, True, True, False]   = HexE
+bitListToHex [True, True, True, True]    = HexF
 bitListToHex _ = error "Invalid BitList"
 
-hexStrToBitList :: String -> BitList
+hexStrToBitList :: HexString -> BitList
 hexStrToBitList = (>>= hexToBitList)
 
 xor :: Bool -> Bool -> Bool
 xor True True = False
 xor a b = a || b
 
-xorStr :: String -> String -> String
+xorStr :: HexString -> HexString -> HexString
 xorStr a b =
     let bitList = zipWith xor (hexStrToBitList a) (hexStrToBitList b)
     in map bitListToHex $ chunksOf 4 bitList
